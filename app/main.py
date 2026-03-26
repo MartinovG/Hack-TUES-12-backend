1beea754bd08f5a145fe9043a9f327e044178c20
yorizova <yoanaantonova9907@gmail.com>
2026-03-26 17:44:54 +0200

    hopes and prayers v2

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(vms.router)
 app.include_router(provider.router)
+
+@app.get("/")
+def root():
+    return {"message": "Backend is running"}
